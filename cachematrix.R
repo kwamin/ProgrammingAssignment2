3f6badfe1127c83fe539d336b4c28e776b7d28e8
Kwamin <kwami@kwaminyamidie.com>
2015-01-25 08:46:16 -0800

    Matrix inversion and GETS method
    This code is inspired by the GETS method Gaussian Elimination Triangular
    Solver method.  I tried to see if a method in MatLab could be adapted to
    solve the cash inversion problem. The code needs a lot of work. But here
    is the direction I would go with the assignment  if I had a good handle
    on functions...I am totally new to programming. Data Toolbox was my
    first programming class, ever.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## My attempt at the second assignment. This is more based on the 
+##theoretical aspect of the assignment, ie., what kind of cube 
+## is invertible and which kind cannot.
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+makeCacheMatrix <- function(x = matrix())### x is an object, a special 
+          ##cube that can be inverted. 
+          #The invertible cube meets the following criteria: 
+          #1. all rows with at least one element different from zero 
+          #above any row of zeros, 2. leading coefficient of a row 
+          #must always be located by necessity to the right of 
+          #the leading coefficient of the row above it. All rows must have a 
+          #leading coefficient different from zero.This will make 
+          #sure that the cube can be changed into a 
+          ##triangular shape and then later inverted back
+          # to a matrix. Gaussian Elimination Triangular Solver method. 
+          #See Jorge Soriano Pinedo's project "Matrix Inversion Speed Up 
+          #With CUDA" IIT Chicago, 2011. {
+                        mcm   <- NULL
+                        set <- function (y)#Here is where I would normally put the code in R 
+                         #to test for a valid cube. 
+                {
+                     x <<- y          ##This is where lexical scoping is happening
+                     mcm <<- NULL
+               }
+      get     <- function() x
+      setcache <- function(cache) mcm <<- cache
+      getcache <- function() mcm
+      list(set = set, get = get,
+     setcache = setcache,
+     getcache = getcache)
+      }
+              cachsolve <- function(x, ...) 
+                {
+              mcm <- x$getcache()
+       if(is.mull(mcm))
+         {
+               message("getting cached data")
+          return(mcm)
+      }
+     {
+            mcm <- solve(mcm) %*% mcm # Note that solve uses per cent asterisk and per cent
+       }data <- x$get()
+           mcm<- cache(data, ...)
+           set$cache(mcm)
+           mcm
+      }
